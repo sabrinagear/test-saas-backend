0e83b6eef72cec8d2d2085105b66c86d7da146f2
Phil Hart <phil@Phils-MacBook-Pro.local>
2019-05-15 16:48:42 -0600

    Refactoring the routes

--- data/helpers/taskDb.js
+++ data/helpers/taskDb.js
@@ -1,58 +1,60 @@
 const db = require("../config");
 
+
+
 // Export functions
 module.exports = {
   get,
   getByGroup,
-  assignedTo,
+  getByUser,
   getById,
   add,
   update,
   remove
-};
+}
 
 function get() {
-  return db("tasks");
+  return db('tasks')
 }
 
 function getByGroup(groupId) {
   return db
-    .select("*")
-    .from("tasks")
-    .where("groupId", groupId);
+    .select('*')
+    .from('tasks')
+    .where('groupID', groupId)
 }
 
-function assignedTo(userId) {
+function getByUser(userId) {
   return db
-    .select("*")
-    .from("tasks")
-    .where("assignedTo", userId);
+    .select('*')
+    .from('tasks')
+    .where('purchasedBy', userId)
 }
 
 function getById(id) {
   return db
-    .select("*")
-    .from("tasks")
-    .where({ id });
+    .select('*')
+    .from('tasks')
+    .where({ id })
 }
 
-function add(item) {
-  return db("tasks")
-    .returning("id")
-    .insert(item)
-    .into("tasks");
+function add(task) {
+  return db('tasks')
+    .returning('id')
+    .insert(task)
+    .into('tasks')
 }
 
 function update(id, changes) {
-  return db("tasks")
-    .returning("id")
+  return db('tasks')
+    .returning('id')
     .where({ id })
-    .update(changes);
+    .update(changes)
 }
 
 function remove(id) {
-  return db("tasks")
-    .returning("id")
+  return db('tasks')
+    .returning('id')
     .where({ id })
-    .del();
+    .del()
 }

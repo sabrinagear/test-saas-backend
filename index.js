3bf4fe2644a1400bd8b066580f9e9d7cbedb8b4e
Farhan Farooqui <farhan@farooqui.net>
2019-05-17 11:46:03 -0700

    update port to env variable

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const server = require("./api/server.js");
 //   require("dotenv").load();
 // }
 
-// const port = process.env.PORT || 9000;
-const port = 9000;
+const port = process.env.PORT
 server.listen(port, () => console.log(`\n Server running on port ${port}. \n`));

1e8c50515433616dbfb287bf9d9fc36c4bc56106
Phil Hart <philhart23@gmail.com>
2019-06-27 13:31:36 -0600

    list routes fixed

--- api/routes/listRoutes.js
+++ api/routes/listRoutes.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const listDb = require("../../data/helpers/listsDb");
+const db = require("../../data/helpers/listsDb");
 
 // POST A LIST //
 
-router.post('/', (req, res) => {
-  const list = req.body
-  listDb
-    .add(list)
-    .then(id => {
-      res.status(200).json({ message: `list successfully added` })
-    })
-    .catch(err => {
-      res.status(500).json({ message: `list could not be added`, err })
-    })
-})
+router.post("/", async (req, res) => {
+  const list = req.body;
+  try {
+    await db.add(list);
+    let updatedArray = await db.get();
+    return res.status(200).json({
+      lists: updatedArray,
+      message: "Successfully Posted"
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+  });
 
 // GET ALL LISTS //
 
 router.get("/", (req, res) => {
-  listDb.get()
+  db.get()
   .then(lists => {
       res.status(200).json(lists);
     })
@@ -30,7 +32,7 @@ router.get("/", (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
-  listDb
+  db
     .getById(id)
     .then(list => {
       if (list.length >= 1) {
@@ -46,52 +48,57 @@ router.get('/:id', (req, res) => {
 
 // DELETE A LIST //
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    let list = await db.get(id);
+    if (!list) {
+      res
+        .status(404)
+        .json({ message: "The list with the specified ID does not exist." });
+    }
+    await db.remove(id);
+    let updatedArray = await db.get();
+    return res.status(200).json({
+      lists: updatedArray,
+      message: "successfully deleted"
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
-  listDb
-    .remove(id)
-    .then(status => {
-      if (status.length >= 1) {
-        return res
-          .status(200)
-          .json({ message: `list successfully deleted.` })
-      } else {
-        return res
-          .status(404)
-          .json({ error: `The requested list does not exist.` })
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: `list could not be deleted`, err })
-    })
-})
+
+
 
 // UPDATE A LIST//
 
-router.put('/:id', (req, res) => {
-  let { id } = req.params
-  let changes = req.body
-  listDb
-    .getById(id)
-    .then(list => {
-      listDb.update(id, changes).then(status => {
-        if (status.length >= 1) {
-          return res
-            .status(200)
-            .json({ message: `list successfully updated.` })
-        } else {
-          return res
-            .status(404)
-            .json({ message: 'The requested list does not exist.' })
-        }
-      })
-    })
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  let changes = req.body;
+try {
+  let list = await db.get(id)
+  if (!list) {
+    res
+    .status(404)
+    .json({ message: "The list with the specified ID does not exist." });
+  }
+  await db.update(id, changes);
+  let updatedArray = await db.get();
+  return res.status(200).json({
+    lists: updatedArray,
+    message: "Successfully Updated"
+  });
+} catch (err) {
+  res.status(500).json(err.message);
+}
+});
 
-    .catch(err => {
-      res.status(500).json({ message: `list could not be updated`, err })
-    })
-})
+
+
+
+
+
 
 
 module.exports = router;

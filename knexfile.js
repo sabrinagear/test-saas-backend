22b59d6c0bba8963b56292e1b9545241e6a3b24a
SabrinaGear <sabrinagear.ft@gmail.com>
2019-07-03 10:50:58 -0500

    variables updated

--- knexfile.js
+++ knexfile.js
@@ -22,10 +22,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: dbConnection + "?ssl=true",
-    pool: {
-      min: 2,
-      max: 10
-    },
+
     migrations: {
       tableName: "knex_migrations",
       directory: "./data/migrations"
